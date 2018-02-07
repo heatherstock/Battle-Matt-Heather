59597ede347695e74db85e66931f740084d3767b
Matt Burstein <j.matthew.burstein@gmail.com>
2018-02-07 14:53:59 +0000

    refactor name_form_spec and player2_hit_points_spec to use new web helper

--- spec/features/name_form_spec.rb
+++ spec/features/name_form_spec.rb
@@ -1,9 +1,6 @@
 feature "name input form" do
   scenario "display welcome message" do
-    visit("/")
-    fill_in :player_1, with: "Heather"
-    fill_in :player_2, with: "Matt"
-    click_button "Submit"
+    sign_in_and_play
     expect(page).to have_content("Heather vs Matt!")
   end
 end
--- spec/features/player2_hit_points_spec.rb
+++ spec/features/player2_hit_points_spec.rb
@@ -1,9 +1,6 @@
 feature "player 2 hit points" do
   scenario "display hit points" do
-    visit("/")
-    fill_in :player_1, with: "Heather"
-    fill_in :player_2, with: "Matt"
-    click_button "Submit"
+    sign_in_and_play
     expect(page).to have_content("Matt: 100HP")
   end
 end
