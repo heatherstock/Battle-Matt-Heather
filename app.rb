e0fd1f26a29292b015af53f14836c24c4444f758
Matt Burstein <j.matthew.burstein@gmail.com>
2018-02-07 15:16:35 +0000

    Add attack player 2 button but not finished logic. run rspec for info

--- app.rb
+++ app.rb
@@ -10,8 +10,8 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    session[:player_1] = params[:player_1]
-    session[:player_2] = params[:player_2]
+    session[:player_1] = params[:player_1] if params.has_key?(:player_1)
+    session[:player_2] = params[:player_2] if params.has_key?(:player_2)
     redirect to('/names')
   end
 
---
+++ spec/features/attack_player2_spec.rb
@@ -0,0 +1,7 @@
+feature "attack player 2" do
+  scenario "displays 'Attack successful!' message" do
+    sign_in_and_play
+    click_button("Attack player 2")
+    expect(page).to have_content("Attack successful!")
+  end
+end
