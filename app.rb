41c796ce11a8a83a6551ca49d8871f8a1075e041
Matt Burstein <j.matthew.burstein@gmail.com>
2018-02-07 14:35:39 +0000

    Refactor post request to /names to redirect to get request. Add session handling

--- app.rb
+++ app.rb
@@ -2,13 +2,22 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
 
+  enable :sessions
+  set :session_secret, 'super secret'
+
   get '/' do
     erb(:index)
   end
 
   post '/names' do
-    @player_1 = params[:player_1]
-    @player_2 = params[:player_2]
+    session[:player_1] = params[:player_1]
+    session[:player_2] = params[:player_2]
+    redirect to('/names')
+  end
+
+  get '/names' do
+    @player_1 = session[:player_1]
+    @player_2 = session[:player_2]
     erb(:names)
   end
 end
