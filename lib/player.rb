f9bd977ae1f95d57759d6383c118a91afbe33638
Daniel Keen <keendaniel01@gmail.com>
2018-02-08 11:48:39 +0000

    Add attack method, which reduces players hp by 10

--- lib/player.rb
+++ lib/player.rb
@@ -3,10 +3,18 @@ class Player
   attr_reader :name, :hp
 
   DEFAULT_HP = 100
+  DEFAULT_DAMAGE = 10
 
   def initialize(name)
     @name = name
     @hp = DEFAULT_HP
   end
 
+  def attack(name)
+    name.take_damage
+  end
+
+  def take_damage
+    @hp -= DEFAULT_DAMAGE
+  end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,12 +1,19 @@
 require "player"
 
 describe Player do
-  subject(:player) {described_class.new("Hilda")}
+  subject(:player) { described_class.new("Hilda") }
+  subject(:player2) { described_class.new("Terry") }
     it "returns player name" do
       expect(player.name).to eq("Hilda")
     end
 
     it "has 100HP when initialized" do
-      expect(player.hp).to eq 100
+      expect(player.hp).to eq described_class::DEFAULT_HP
+    end
+
+    describe '#attack' do
+      it 'lowers a players hp by 10' do
+        expect { player.attack(player2) }.to change { player2.hp }.by(-described_class::DEFAULT_DAMAGE)
+      end
     end
 end
