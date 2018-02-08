addf7346f197d71ba6ed1c2abca30ecf68c5507d
Heather Stock <33641095+heatherstock@users.noreply.github.com>
2018-02-08 10:35:35 +0000

    Deleted Matt's progress

--- app.rb
+++ app.rb
@@ -4,29 +4,24 @@ require_relative './lib/player.rb'
 class Battle < Sinatra::Base
 
   enable :sessions
-  set :session_secret, 'super secret'
 
   get '/' do
     erb(:index)
   end
 
   post '/names' do
-    $player_1 = Player.new(params[:player_1])
-    $player_2 = Player.new(params[:player_2])
+    session[:player_1] = params[:player_1]
+    session[:player_2] = params[:player_2]
     redirect to('/play')
   end
 
   get '/play' do
-    @player_1 = $player_1.name
-    @player_2 = $player_2.name
-    @player_2_hp = $player_2.hitpoints
+    @player_1 = session[:player_1]
+    @player_2 = session[:player_2]
     erb(:play)
   end
 
   get '/attack' do
-    $player_1.attack($player_2)
-    @player_2 = $player_2.name
-    @player_2_hp = $player_2.hitpoints
     erb(:attack)
   end
 end
